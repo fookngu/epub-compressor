cffb52d7b83432b7ac0d2bd3efa3f3daae8c19d6
phucngu <phucnn.work@gmail.com>
2024-01-04 10:09:55 +0000

    Initial setup with Java 21 and Spring 3.2.1, only the spring web dependency

---
+++ src/main/java/net/toiditimtoi/epubcompressor/EpubCompressorApplication.java
@@ -0,0 +1,13 @@
+package net.toiditimtoi.epubcompressor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpubCompressorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpubCompressorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/toiditimtoi/epubcompressor/EpubCompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.toiditimtoi.epubcompressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpubCompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
