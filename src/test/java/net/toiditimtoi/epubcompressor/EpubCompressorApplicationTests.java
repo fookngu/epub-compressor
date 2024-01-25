f72af0824abd78681049a0ae572d4862898097a2
phucngu <phucnn.work@gmail.com>
2024-01-26 00:34:43 +0800

    WIP: testing image compressor using Java image utilities

--- src/test/java/net/toiditimtoi/epubcompressor/EpubCompressorApplicationTests.java
+++ src/test/java/net/toiditimtoi/epubcompressor/EpubCompressorApplicationTests.java
@@ -1,13 +1,26 @@
 package net.toiditimtoi.epubcompressor;
 
+import net.toiditimtoi.epubcompressor.imagecompressor.EpubExtractor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 @SpringBootTest
 class EpubCompressorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		try {
+			Path tempDir = EpubExtractor.extractEpub("gitignorethis/DK - A Short History of the Vietnam War-DK Publishing (2021).epub");
+			System.out.println("Extracted to: " + tempDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
